30bee7966c062166d99acf2468eee13d3b88cbe8
Hugaqq <qiaoqifan@huawei.com>
2026-09-10 14:23:19 +0800

    fix: add nvtx range in code which is for benchmarking specific parts in code

--- assignment2-systems/cs336_systems/benchmark.py
+++ assignment2-systems/cs336_systems/benchmark.py
@@ -1,10 +1,41 @@
 import torch
 import argparse
+import cs336_basics
+import einx
+import math
+from einops import einsum, rearrange
+from cs336_basics.nn_utils import softmax
+import torch.cuda.nvtx as nvtx
+from torch import Tensor
 from timeit import default_timer
+from jaxtyping import Float, Int, Bool
 from cs336_systems.config import Config
 from cs336_basics.model import BasicsTransformerLM
 from cs336_basics.optimizer import AdamW
 from cs336_basics.nn_utils import cross_entropy
+
+@nvtx.range("scaled dot product attention")
+def annotated_scaled_dot_product_attention(
+   Q: Float[Tensor, "... queries d_k"],
+   K: Float[Tensor, "... keys    d_k"],
+   V: Float[Tensor, "... keys    d_v"],
+   mask: Bool[Tensor, "... queries d_v"] | None = None,
+) -> Float[Tensor, "... queries d_v"]:
+   d_k = K.shape[-1]
+   with nvtx.range("computing attention scores"):
+      attention_scores = einsum(Q, K, "... queries d_k, ... keys d_k -> ... queries keys") / math.sqrt(d_k)
+
+   if mask is not None:
+      attention_scores = torch.where(mask, attention_scores, float("-inf"))
+
+   with nvtx.range("computing softmax"):
+      attention_weights = softmax(attention_scores, dim = -1)
+   
+   with nvtx.range("final matmul"):
+      res = einsum(attention_weights, V, "... queries keys, ... keys d_v -> ... queries d_v")
+   return res
+
+cs336_basics.model.scaled_dot_product_attention = annotated_scaled_dot_product_attention
 
 def benchmark_test(
       running_config: Config,
@@ -13,27 +44,28 @@ def benchmark_test(
       full: bool = False
       ):
    benchmark_transformer_lm = BasicsTransformerLM(running_config.vocab_size, running_config.context_length, running_config.d_model, running_config.num_layers, running_config.num_heads, running_config.d_ff).to(running_config.device, running_config.dtype)
+
    opt = AdamW(benchmark_transformer_lm.parameters())
    x = torch.randint(0, running_config.vocab_size, size = (running_config.batch_size, running_config.context_length)).to(device = running_config.device)
    targets = torch.randint(0, running_config.vocab_size, size = (running_config.batch_size, running_config.context_length)).to(device = running_config.device)
    
-   
-   for _ in range(running_config.warm_up_times):
-      if forward_only is False:
-         opt.zero_grad()
-      res = benchmark_transformer_lm(x)
-      if forward_only is False:
-         loss = cross_entropy(res, targets)
-         loss.backward()
-      if full is True:
-         opt.step()
+   with nvtx.range("warm-up"):
+      for _ in range(running_config.warm_up_times):
+         if forward_only is False:
+            opt.zero_grad()
+         res = benchmark_transformer_lm(x)
+         if forward_only is False:
+            loss = cross_entropy(res, targets)
+            loss.backward()
+         if full is True:
+            opt.step()
 
-   torch.cuda.synchronize()
+   torch.cuda.synchronize(device = running_config.device)
 
    t_whole = []
 
-   for step in range(running_config.tests):
-      torch.cuda.synchronize()
+   for _ in range(running_config.tests):
+      torch.cuda.synchronize(device = running_config.device)
       t0 = default_timer()
       if forward_only:
          res = benchmark_transformer_lm(x)
@@ -51,7 +83,7 @@ def benchmark_test(
          loss.backward()
          opt.step()
 
-      torch.cuda.synchronize()
+      torch.cuda.synchronize(device = running_config.device)
       t1 = default_timer()
       t_whole.append(t1 - t0)
    
@@ -60,9 +92,6 @@ def benchmark_test(
    std_t = t_whole.std()
    print(f"test results: mean_t :{mean_t}")
    print(f"              std_t :{std_t}")
-
-
-
 
 def main():
    parser = argparse.ArgumentParser()
